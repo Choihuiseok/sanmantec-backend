1244e84e502d386741e8a9927761944279f55d8e
Choihuiseok <noanlove1@gmail.com>
2025-11-28 15:44:37 +0900

    오타 수정

--- api/routes/wallet.js
+++ api/routes/wallet.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("../controllers/walletController");
 
 router.post("/save", controller.saveWallet);
-router.get("/:userId", controller.getWallets);
+router.get("/:userId", controller.getWallet);
 router.post("/delete", controller.deleteWallet);
 
 module.exports = router;
